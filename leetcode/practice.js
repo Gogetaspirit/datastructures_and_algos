6df678a03eefe36f8f0b9272cb97d97f1f946270
ChrisLee <christopher.dan.lee96@gmail.com>
2021-01-25 17:26:55 -0500

    leetcode merge intervals problem complete

--- leetcode/practice.js
+++ leetcode/practice.js
@@ -40,3 +40,23 @@ const addTwoNumbers = function(l1, l2) {
     }
     return result.next;
 };
+
+
+//Merge Intervals
+//Given an array of intervals where intervals[i] = [starti, endi], 
+//merge all overlapping intervals, and return an array of the non-overlapping intervals 
+//that cover all the intervals in the input.
+
+var merge = function (intervals) {
+    intervals.sort((a, b) => a[0] - b[0])
+    const res = [intervals[0]]
+    for (let curr of intervals) {
+        prev = res[res.length - 1]
+        if (prev[1] >= curr[0]) {
+            prev[1] = Math.max(curr[1], prev[1])
+        } else {
+            res.push(curr)
+        }
+    }
+    return res
+};
