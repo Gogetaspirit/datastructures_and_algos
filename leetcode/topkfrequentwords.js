eac4fac8c0fa8454fa6b8eca1340ca938d48b7fe
ChrisLee <christopher.dan.lee96@gmail.com>
2021-01-27 15:42:18 -0500

    finished top k frequent words problem

---
+++ leetcode/topkfrequentwords.js
@@ -0,0 +1,19 @@
+//Given a non-empty list of words, return the k most frequent elements.
+//Your answer should be sorted by frequency from highest to lowest.
+//If two words have the same frequency, then the word with the lower alphabetical order comes first.
+
+var topKFrequent = function (words, k) {
+    const mappings = {}
+    for (let i = 0; i < words.length; i++) {
+        mappings[words[i]] = mappings[words[i]] + 1 || 1
+    }
+    const sorted = Object.keys(mappings).sort((a, b) => {
+        if (mappings[b] === mappings[a]) {
+            return a > b ? 1 : -1
+        }
+        return mappings[b] - mappings[a]
+    })
+    return sorted.slice(0, k)
+
+
+};
