ff4ffa8238c3c166c869fec21ef4aa4bc36b0a6f
ChrisLee <christopher.dan.lee96@gmail.com>
2021-02-15 19:18:28 -0500

    sum of oddlength subarrs in O(n) time

---
+++ leetcode/sumofoddlengthsubarr.js
@@ -0,0 +1,11 @@
+//Given an array of positive integers arr, calculate the sum of all possible odd - length subarrays.
+//A subarray is a contiguous subsequence of the array.
+//Return the sum of all odd - length subarrays of arr.
+
+var sumOddLengthSubarrays = function (arr) {
+    let res = 0, n = arr.length
+    for (let i = 0; i < n; ++i) {
+        res += parseInt(((i + 1) * (n - i) + 1) / 2) * arr[i];
+    }
+    return res
+};
