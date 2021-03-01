5caadb219b3f51653635082e4474752ea12a0a3e
ChrisLee <christopher.dan.lee96@gmail.com>
2021-03-01 17:56:24 -0500

    subsequence two pointer approach

---
+++ leetcode/subsequence.js
@@ -0,0 +1,21 @@
+//Given two strings s and t, check if s is a subsequence of t.
+//A subsequence of a string is a new string that is formed from the original string by deleting some(can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+
+
+var isSubsequence = function (s, t) {
+    let i = 0;
+    let m = 0;
+
+    while (m < t.length) {
+        if (s[i] === t[m]) {
+            i++;
+            m++;
+        }
+        else {
+            m++;
+        }
+    }
+    return i === s.length
+
+};
