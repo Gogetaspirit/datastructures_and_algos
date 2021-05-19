8f6e93bb9374d77d409f82765f32d54f35924234
ChrisLee <christopher.dan.lee96@gmail.com>
2021-05-18 23:51:50 -0400

    graph traversal python recursion

--- pythonpractice/learningpython.py
+++ pythonpractice/learningpython.py
@@ -129,3 +129,10 @@ def mergeOverlappingIntervals(intervals):
 			result.append(currentInterval)
 		idx += 1
 	return result
+
+def numberOfWaysToTraverseGraph(width, height):
+    # Write your code here.
+    if width is 1 or height is 1:
+		return 1
+	return numberOfWaysToTraverseGraph(width-1, height) + numberOfWaysToTraverseGraph(width, height-1)
+	
