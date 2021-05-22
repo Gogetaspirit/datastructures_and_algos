61b7bfe12374ba8a067e78ad5dd091dfdcf51151
ChrisLee <christopher.dan.lee96@gmail.com>
2021-05-22 14:19:42 -0400

    recursive python mnemonics

--- pythonpractice/algoexpertmed.py
+++ pythonpractice/algoexpertmed.py
@@ -10,3 +10,37 @@ def powerset(array, left = None):
 		currentSubset = subsets[idx]
 		subsets.append(currentSubset + [ele])
 	return subsets
+
+def phoneNumberMnemonics(phoneNumber, idx = 0, emptyStr = '', result = []):
+    # Write your code here.
+	current = ['0'] * len(phoneNumber)
+	found = []
+	
+	recursiveHelper(0, phoneNumber, current, found)
+	return found
+
+def recursiveHelper(idx, phoneNumber, current, found):
+	if idx == len(phoneNumber):
+		mnemonic = ''.join(current)
+		found.append(mnemonic)
+	else:
+		digit = phoneNumber[idx]
+		letters = MAP[digit]
+		for letter in letters:
+			current[idx] = letter
+			recursiveHelper(idx + 1, phoneNumber, current, found)
+		
+	
+MAP = {
+		"0": ["0"],
+		"1": ["1"],
+		"2": ["a", "b", "c"],
+		"3": ["d", "e", "f"],
+		"4": ["g", "h", "i"],
+		"5": ["j", "k", "l"],
+		"6": ["m", "n", "o"],
+		"7": ["p", "q", "r", "s"],
+		"8": ["t", "u", "v"],
+		"9": ["w", "x", "y", "z"]
+}
+	
